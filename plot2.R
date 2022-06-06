81a75634d3380fcdacba0e51c48c778373d7921d
Ayako Nagao <belanello@live.com>
2022-06-06 10:22:58 +0900

    all R code files and png files added

---
+++ plot2.R
@@ -0,0 +1,41 @@
+library(dplyr)
+library(lubridate)
+
+# Load zip file if there is no downloaded data
+if (!exists('data_init')){
+  url <- 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
+  download.file(url, 'data.zip')
+  
+  # Unzip file and read text file
+  unzip('data.zip')
+  data_init <- read.table('household_power_consumption.txt',header = TRUE,
+                          sep = ';')
+  
+  # Subset the data
+  # Combine Date and Time and make new column 'DT'
+  # Change data in character to Numeric
+  
+  data <- data_init %>% 
+    subset((Date=='1/2/2007'|Date=='2/2/2007')) %>%
+    mutate(DT=paste(Date,Time)) %>% # paste together Date and Time
+    mutate(DT=dmy_hms(DT)) %>% # Change them into Date class and assign them to new column
+    mutate(Global_active_power=as.numeric(Global_active_power), # Change data in chr to numeric
+           Global_reactive_power=as.numeric(Global_reactive_power),
+           Voltage=as.numeric(Voltage),
+           Global_intensity=as.numeric(Global_intensity),
+           Sub_metering_1=as.numeric(Sub_metering_1),
+           Sub_metering_2=as.numeric(Sub_metering_2),
+           Sub_metering_3=as.numeric(Sub_metering_3),) 
+}
+
+
+# Plot and save it as png file
+plot.new()
+with(data, plot(DT,
+                Global_active_power,
+                type='l',
+                xlab='',
+                ylab='Global Active Power (kilowatts)'))
+
+dev.copy(png,'plot2.png')
+dev.off()      
